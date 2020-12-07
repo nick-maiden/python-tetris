421bdd94784ddcc3978a46ee09a17e899010c54a
Nicholas Maiden <nicholas.maiden@uon.edu.au>
2020-12-07 11:34:53 +1100

    falling block now working

--- tetris.py
+++ tetris.py
@@ -8,43 +8,55 @@ min_grid_width = 0
 game_run = 1
 STRAIGHT_BLOCK_X = [7, 6, 5, 4]
 STRAIGHT_BLOCK_Y = [0, 0, 0, 0]
+block_x = 0
+block_y = 0
 
 system("clear")
 screen = curses.initscr()
 curses.noecho()
 curses.cbreak()
+curses.curs_set(0)
+
+def draw_block(STRAIGHT_BLOCK_X, STRAIGHT_BLOCK_Y, block_x, block_y):
+    i = 0
+    while i < len(STRAIGHT_BLOCK_X):
+        screen.addstr(STRAIGHT_BLOCK_Y[i] + block_y, STRAIGHT_BLOCK_X[i] + block_x, "*")
+        i += 1
 
 
-def printscreen():
+
+def print_background():
+    global y
     y = min_grid_height
-    screen.clear()
     while y < max_grid_height:
 
+        global x
         x = min_grid_width
         while x < max_grid_width:
 
             screen.addstr(y, x, "-")
 
-            i = 0
-            while i < len(STRAIGHT_BLOCK_X):
-
-                if x == STRAIGHT_BLOCK_X[i] and y == STRAIGHT_BLOCK_Y[i]:
-
-                    screen.addstr(y, x, "*")
-
-                i += 1
-
             x += 1
 
         y += 1
-    screen.refresh()
 
 while game_run == 1:
-    printscreen()
+    screen.clear()
+    print_background()
+    draw_block(STRAIGHT_BLOCK_X, STRAIGHT_BLOCK_Y, block_x, block_y)
+    screen.refresh()
     system ("sleep 1")
-    i = 0
-    while i < len(STRAIGHT_BLOCK_Y):
-        STRAIGHT_BLOCK_Y[i] = STRAIGHT_BLOCK_Y[i] + 1
-        if STRAIGHT_BLOCK_Y[i] == max_grid_height:
-            break
-        i += 1
+    if block_y < max_grid_height - 1:
+        block_y += 1
+    else:
+        block_y = max_grid_height - 1
+
+
+    screen.nodelay(True)
+    try:
+        move = screen.getkey()
+    except:
+        move = ""
+
+    if move == "q":
+        game_run = 0
